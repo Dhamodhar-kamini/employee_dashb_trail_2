ae35e29115e11db182d94f148d503a3e628a5bd1
vanipenta saleem <vanipentashaiksaleem@gmail.com>
2026-03-14 17:17:46 +0530

    update2

--- pages/approval/approval.js
+++ pages/approval/approval.js
@@ -2,7 +2,7 @@
 // 1. CONFIGURATION & GLOBAL VARIABLES
 // ==========================================
 
-const API_BASE_URL = "http://13.60.70.185:8000";
+const API_BASE_URL = "http://13.51.167.95:8000";
 
 // ==========================================
 // 2. UI & NAVIGATION LOGIC
